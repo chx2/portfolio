65d762bd4ef1771aee365ed96ee3c095fc1ea34e
Chris H <crishack8@gmail.com>
2022-03-21 23:47:24 -0500

    Updates homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="md:flex">
-        <div class="bg-gray-900 z-0 w-full shadow-2xl rounded-r md:w-1/2 p-16 flex justify-center items-center flex-col text-white animate__animated animate__slideInLeft">
-            <h1 class="text-4xl mb-4">Hello, I'm Chris.</h1>
-            <h2 class="text-2xl animate__animated animate__fadeInLeft animate__delay-1s">Full-Stack Developer</h2>
-            <div class="mt-4 flex">
-                <a class="cursor-pointer transition duration-100 hover:scale-125" href="mailto:crishack8@gmail.com">
-                    <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInUp animate__delay-1s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
-                </a>
-                <a class="cursor-pointer transition duration-100 hover:scale-125" href="https://github.com/chx2/">
-                    <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInUp animate__delay-2s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
-                </a>
-                <a class="cursor-pointer transition duration-100 hover:scale-125" href="https://www.linkedin.com/in/chris-o-hackett/">
-                    <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInUp animate__delay-3s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />  <rect x="2" y="9" width="4" height="12" />  <circle cx="4" cy="4" r="2" /></svg>
-                </a>
+        <div class="w-full md:w-1/2">
+            <div class="bg-gray-900 z-0 rounded-r p-16 flex justify-center items-center flex-col text-white animate__animated animate__slideInLeft">
+                <h1 class="text-4xl mb-4">Hello, I'm Chris.</h1>
+                <h2 class="text-2xl animate__animated animate__fadeInUp animate__delay-1s">Full-Stack Developer</h2>
+                <div class="mt-4 flex">
+                    <a class="cursor-pointer transition duration-100 hover:scale-125" href="mailto:crishack8@gmail.com">
+                        <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInUp animate__delay-1s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
+                    </a>
+                    <a class="cursor-pointer transition duration-100 hover:scale-125" href="https://github.com/chx2/">
+                        <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInLeft animate__delay-2s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
+                    </a>
+                    <a class="cursor-pointer transition duration-100 hover:scale-125" href="https://www.linkedin.com/in/chris-o-hackett/">
+                        <svg class="h-8 w-8 m-2 text-white animate__animated animate__fadeInRight animate__delay-3s"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />  <rect x="2" y="9" width="4" height="12" />  <circle cx="4" cy="4" r="2" /></svg>
+                    </a>
+                </div>
+            </div>
+            <div class="slider animate__animated animate__fadeInRight animate__delay-3s">
+                <div class="slide-track">
+                    <div class="slide" v-for="(skill, skillIndex) in sort(skills[0].data)" :key="skillIndex">
+                        <img :src="`/images/skills/${skill.thumbnail}`" :alt="skill.name" />
+                    </div>
+                </div>
             </div>
         </div>
         <div class="w-full md:w-1/2">
@@ -26,6 +35,17 @@ import "animate.css"
 
 export default {
     layout: 'index',
+    async asyncData ({ $content }) {
+        const skills = await $content('data').where({ name: 'Skills' }).limit(1).fetch()
+        return {
+            skills
+        }
+    },
+    methods: {
+        sort(array) {
+            return array.sort(() => Math.random() - 0.5)
+        }
+    },
     head () {
       return {
         title: "Home",
@@ -88,3 +108,54 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+@keyframes scroll {
+    0% { transform: translateX(0); }
+    100% { transform: translateX(calc(-250px * 7))}
+}
+
+.slider {
+    background: transparent;
+    height: 100px;
+    margin: 48px auto auto auto;
+    overflow:hidden;
+    position: relative;
+
+    &::before,
+    &::after {
+        content: "";
+        height: 100px;
+        position: absolute;
+        width: 200px;
+        z-index: 2;
+    }
+
+    &::after {
+        right: 0;
+        top: 0;
+        transform: rotateZ(180deg);
+    }
+
+    &::before {
+        left: 0;
+        top: 0;
+    }
+
+    .slide-track {
+        height: 100px;
+        animation: scroll 30s linear infinite;
+        animation-direction: alternate;
+        display: flex;
+        width: calc(250px * 14);
+    }
+
+    .slide {
+        padding-top: 8px;
+        margin-right: 32px;
+        height: 80px;
+        width: 80px;
+        z-index: 999;
+    }
+}
+</style>
